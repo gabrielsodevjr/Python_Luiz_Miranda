a7c207879e6f73574fd30d95ae29997708ebd076
gabrielsodevjr <gabrielso.devjr@gmail.com>
2025-02-08 02:52:19 -0300

    modified:   exercicios/aula48.py 	new file:   exercicios/aula49_50.py 	new file:   exercicios/aula51.py 	new file:   exercicios/aula52.py 	new file:   exercicios/aula53.py 	new file:   exercicios/aula54.py

---
+++ exercicios/aula49_50.py
@@ -0,0 +1,9 @@
+"""
+for in com listas
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+contador = 0
+
+for nome in lista:
+    print(nome, ", indice:", contador)
+    contador = contador + 1
---
+++ exercicios/aula51.py
@@ -0,0 +1,5 @@
+"""
+Introdução ao empacotamento e desempacotamento
+"""
+_[1,5], nome, *resto = ['Maria', 'Helena', 'Luiz']
+print(nome)
